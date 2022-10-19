c76a3dbb1575de9a559560e26a681b610352ef56
TaoLoading <1377226380@qq.com>
2022-10-19 11:44:14 +0800

    组件中文化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
+// 使用中文
+const locale = zhCN
+dayjs.locale('zh-cn')
+
 </script>
